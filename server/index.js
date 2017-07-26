86452aca9d7eb9f6224c59ce37f6334c671b8e7e
Mike Doyle <mikedoyle007@gmail.com>
2017-07-25 19:29:55 -0700

    Start server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 // require necessary imports
+const express = require('express');
+const parser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(parser.urlencoded({ extended: true }));
+
+
 
 // have express instance listen in on a PORT
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('there was an error in the server');
+  } else {
+    console.log('successfull - server');
+  }
+})
