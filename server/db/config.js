63dbcccdcb0ce3ed738e95d7e9885bfccadf9bb5
Mike Doyle <mikedoyle007@gmail.com>
2017-07-25 19:44:42 -0700

    Postgress

--- server/db/config.js
+++ server/db/config.js
@@ -1,1 +1,9 @@
 // create connection to db
+const Sequelize = require('sequelize');
+
+const db = new Sequelize('postgres://afnrfkap:hgaexNf3UKGwHsWuFqvjh6ksERM6DQkZ@babar.elephantsql.com:5432/afnrfkap', {
+  dialect: 'postgress',  
+});
+console.log('Connected to remote db');
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -4,17 +4,18 @@ const parser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
+const db = require('./db/config');
 
 // middleware
 app.use(parser.urlencoded({ extended: true }));
-
+app.use(parser.json());
 
 
 // have express instance listen in on a PORT
 app.listen(PORT, (err) => {
   if (err) {
-    console.log('there was an error in the server');
+    console.log('there was an error in the server', err);
   } else {
-    console.log('successfull - server');
+    console.log('successfull - server', PORT);
   }
 })
